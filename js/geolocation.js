3b83fdbf7f5f6b601a6a6dcd1074951870cf5f40
Sunil Shastry <sunil.shastry69@gmail.com>
2020-12-26 18:18:58 +0530

    New permission feature added -Seconds

--- js/geolocation.js
+++ js/geolocation.js
@@ -7,7 +7,18 @@ const stateDistrict = document.getElementById("state-district");
 const postCode = document.getElementById("post-code");
 const stcCode = document.getElementById("stc-code");
 
-window.navigator.geolocation.getCurrentPosition(async function (pos) {
+// prettier-ignore
+const permissionOverlayContainer = document.querySelector(".permission-overlay");
+const permissionCloseBtn = document.getElementById("permission-close");
+
+window.navigator.geolocation.getCurrentPosition(
+	getGeolocationDetails,
+	function () {
+		permissionOverlayContainer.classList.remove("permission-hidden");
+	},
+);
+
+async function getGeolocationDetails(pos) {
 	if (pos) {
 		const { coords } = pos;
 		const { latitude, longitude } = coords;
@@ -21,12 +32,16 @@ window.navigator.geolocation.getCurrentPosition(async function (pos) {
 			results[0].components.state_code
 		} - ${results[0].components.country_code.toUpperCase()}`;
 	}
-});
+}
 
 async function fetchUserLocation(lat, long) {
 	const res = await fetch(
-		`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=a81a7f29b99b4ee686bb98e570ef1204`
+		`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=a81a7f29b99b4ee686bb98e570ef1204`,
 	);
 	const data = await res.json();
 	return data;
 }
+
+permissionCloseBtn.addEventListener("click", function () {
+	permissionOverlayContainer.classList.add("permission-hidden");
+});
