9aaf7bb38f5609292165d0b0b3b604ed0de3eac8
sunil <sunil.shastry69@gmail.com>
2025-02-22 12:56:11 -0600

    Formatted the JS modules on autosave settings

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Main Script
 
-const taskbarBtn = document.getElementById("taskbar-btn");
+const taskbarBtn = document.getElementById('taskbar-btn');
 
-taskbarBtn.addEventListener("click", function () {
-	document.querySelector(".taskbar-icon").classList.toggle("icon-tgl");
-	this.classList.toggle("taskbar-tgl");
-	if (this.classList.contains("taskbar-tgl")) {
-		document.querySelector(".taskbar-text").textContent = "Less";
-		document.querySelector(".extra-notifications").classList.add("open-noti");
-		document.querySelector(".quote-container").classList.add("quote-hide");
-		document.querySelector(".greeting-container").classList.add("greet-hide");
-	} else {
-		document.querySelector(".taskbar-text").textContent = "More";
-		document
-			.querySelector(".extra-notifications")
-			.classList.remove("open-noti");
-		document.querySelector(".quote-container").classList.remove("quote-hide");
-		document
-			.querySelector(".greeting-container")
-			.classList.remove("greet-hide");
-	}
+taskbarBtn.addEventListener('click', function () {
+  document.querySelector('.taskbar-icon').classList.toggle('icon-tgl');
+  this.classList.toggle('taskbar-tgl');
+  if (this.classList.contains('taskbar-tgl')) {
+    document.querySelector('.taskbar-text').textContent = 'Less';
+    document.querySelector('.extra-notifications').classList.add('open-noti');
+    document.querySelector('.quote-container').classList.add('quote-hide');
+    document.querySelector('.greeting-container').classList.add('greet-hide');
+  } else {
+    document.querySelector('.taskbar-text').textContent = 'More';
+    document
+      .querySelector('.extra-notifications')
+      .classList.remove('open-noti');
+    document.querySelector('.quote-container').classList.remove('quote-hide');
+    document
+      .querySelector('.greeting-container')
+      .classList.remove('greet-hide');
+  }
 });
