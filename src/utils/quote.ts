9369db11ac5b0f8625f3db3e2dc92219de7e690b
sunil <sunil.shastry69@gmail.com>
2025-02-26 17:35:44 -0600

    Integrate zStr zod schema and QuoteResponse type to getRandomQuote method in quote.ts

--- src/utils/quote.ts
+++ src/utils/quote.ts
@@ -1,6 +1,7 @@
 import { zStr } from '../schema/zStr';
+import QuoteResponse from '../types/QuoteResponse';
 
-async function getRandomQuote(quoteUrl: string | undefined) {
+async function getRandomQuote(quoteUrl: string | undefined): QuoteResponse {
 	if (zStr.parse(quoteUrl)) {
 		const responsePromise = await fetch(quoteUrl as string);
 		const { author, content } = await responsePromise.json();
