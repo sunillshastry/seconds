944bfff998086db31078f89e4abded3af1ad7225
sunil <sunil.shastry69@gmail.com>
2025-03-03 19:48:51 -0600

    Develop code for fetchUserCurrentLocation function in geolocation.ts source file, removed unnecessary elements and variables

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,7 +1,3 @@
-// const geodataURLEndpoint =
-// 	import.meta.env.VITE_GEODATA_ENDPOINT ||
-// 	'https://nominatim.openstreetmap.org/reverse';
-
 async function fetchUserCurrentLocation(
 	URL: string,
 	latitude: number,
@@ -10,7 +6,6 @@ async function fetchUserCurrentLocation(
 	const queriedUrlEndpoint = `${URL}?lat=${latitude}&lon=${longitude}&format=json&extratags=1&namedetails=1`;
 	const fetchResponse = await fetch(queriedUrlEndpoint);
 	const responseData = await fetchResponse.json();
-	console.log(responseData);
 	return responseData;
 }
 
