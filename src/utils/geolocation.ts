12aedc05529f8ace2e10f4e609c324c89e2e96dc
sunil <sunil.shastry69@gmail.com>
2025-03-03 22:42:11 -0600

    Replace core fetch syntax with axios.get method

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,12 +1,13 @@
+import axios from 'axios';
+
 async function fetchUserCurrentLocation(
 	URL: string,
 	latitude: number,
 	longitude: number
 ) {
 	const queriedUrlEndpoint = `${URL}?lat=${latitude}&lon=${longitude}&format=json&extratags=1&namedetails=1`;
-	const fetchResponse = await fetch(queriedUrlEndpoint);
-	const responseData = await fetchResponse.json();
-	return responseData;
+	const fetchResponse = await axios.get(queriedUrlEndpoint);
+	return fetchResponse.data;
 }
 
 export { fetchUserCurrentLocation };
