44b1e5b412ad7d9c8a180e00e939ee5eb6c7dc80
sunil <sunil.shastry69@gmail.com>
2025-03-03 19:41:14 -0600

    Implement code for fetchUserCurrentLocation function: still incomplete - missing some types and documentation

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,1 +1,17 @@
-console.log('Hello from Geolocation.ts!');
+// const geodataURLEndpoint =
+// 	import.meta.env.VITE_GEODATA_ENDPOINT ||
+// 	'https://nominatim.openstreetmap.org/reverse';
+
+async function fetchUserCurrentLocation(
+	URL: string,
+	latitude: number,
+	longitude: number
+) {
+	const queriedUrlEndpoint = `${URL}?lat=${latitude}&lon=${longitude}&format=json&extratags=1&namedetails=1`;
+	const fetchResponse = await fetch(queriedUrlEndpoint);
+	const responseData = await fetchResponse.json();
+	console.log(responseData);
+	return responseData;
+}
+
+export { fetchUserCurrentLocation };
