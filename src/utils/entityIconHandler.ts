e26ea85dce2543e63f0bcfbb7db9e9f60881396c
sunil <sunil.shastry69@gmail.com>
2025-03-03 21:47:54 -0600

    Setup entityIconHandler function with 'if' rules and imports with default Icon

--- src/utils/entityIconHandler.ts
+++ src/utils/entityIconHandler.ts
@@ -1,7 +1,30 @@
+import GeoData from '../schema/GeoData';
+import { z } from 'zod';
+import IconObjectKey from '../types/IconObjectKey';
+
 const SVGInfoCircle = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
 </svg>`;
 
-function entityIconHandler(objectKey: string) {}
+function entityIconHandler(objectKey: IconObjectKey) {
+	if (objectKey === 'house_number') {
+		return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+  <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+</svg>`;
+	} else if (objectKey === 'road') {
+		return 'road';
+	} else if (objectKey === 'neighbourhood') {
+	} else if (objectKey === 'suburb') {
+	} else if (objectKey === 'city') {
+	} else if (objectKey === 'state') {
+	} else if (objectKey === 'postcode') {
+	} else if (objectKey === 'country') {
+	} else if (objectKey === 'country_code') {
+	} else if (objectKey === 'ISO3166-2-lvl4') {
+	} else if (objectKey === 'county') {
+	} else if (objectKey === 'state_district') {
+	}
+}
 
 export default entityIconHandler;
