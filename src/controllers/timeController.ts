e6dab571405b2dd5920bf5552c70b6b663673672
sunil <sunil.shastry69@gmail.com>
2025-03-04 00:17:21 -0600

    Display greeting message via timeController.ts

--- src/controllers/timeController.ts
+++ src/controllers/timeController.ts
@@ -8,12 +8,20 @@ const DOMGreetingDate = document.querySelector(
 	'#greeting--date'
 )! as HTMLHeadingElement;
 
+const DOMGreetingMessage = document.querySelector(
+	'.greeting--primary'
+)! as HTMLHeadingElement;
+
 function setTimeToUserUI() {
 	const currentUserTime = getUserTime();
 	const userTimeZodParse = GetUserTime.safeParse(currentUserTime);
 	if (userTimeZodParse.success) {
-		const { date, month, year, hours, minutes, isPostMeridian } =
+		const { date, month, year, hours, minutes, isPostMeridian, greeting } =
 			userTimeZodParse.data;
+
+		// Set the greeting message
+		DOMGreetingMessage.textContent = `Good ${greeting}, it is currently`;
+
 		// Set the current time
 		DOMGreetingTime.textContent = `${hours < 10 ? `0${hours}` : hours}:${
 			minutes < 10 ? `0${minutes}` : minutes
