6d99945255506ebbdc65db0b14f4ba8f530075a3
sunil <sunil.shastry69@gmail.com>
2025-03-03 22:43:17 -0600

    Replace core fetch syntax with axios.get method in quote.ts file

--- src/utils/quote.ts
+++ src/utils/quote.ts
@@ -1,10 +1,11 @@
 import { zStr } from '../schema/zStr';
 import QuoteResponse from '../types/QuoteResponse';
+import axios from 'axios';
 
 async function getRandomQuote(quoteUrl: string | undefined): QuoteResponse {
 	if (zStr.parse(quoteUrl)) {
-		const responsePromise = await fetch(quoteUrl as string);
-		const { author, content } = await responsePromise.json();
+		const responsePromise = await axios.get(quoteUrl as string);
+		const { author, content } = responsePromise.data;
 		return { author, content };
 	} else {
 		return null;
