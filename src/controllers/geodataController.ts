949665243da2a8e54ecc9879a49f406fb52c9b86
sunil <sunil.shastry69@gmail.com>
2025-03-03 20:26:47 -0600

    Implement code for geoDataController.ts source file: handles success and fail cases; sets up UI for user after success

--- src/controllers/geodataController.ts
+++ src/controllers/geodataController.ts
@@ -1,16 +1,48 @@
-// const geodataURLEndpoint =
-// 	import.meta.env.VITE_GEODATA_ENDPOINT ||
-// 	'https://nominatim.openstreetmap.org/reverse';
-
+import { z } from 'zod';
 import { fetchUserCurrentLocation } from '../utils/geolocation';
+import GeoData from '../schema/GeoData';
+
+const geoDataURLEndpoint =
+	import.meta.env.VITE_GEODATA_ENDPOINT ||
+	'https://nominatim.openstreetmap.org/reverse';
+
+const DOMGeolocationContainer = document.querySelector(
+	'.geolocation'
+)! as HTMLElement;
 
 window.navigator.geolocation.getCurrentPosition(
 	handleGeoNavigateSuccess,
 	handleGeoNavigateFailure
 );
 
-function handleGeoNavigateSuccess(position) {}
+async function handleGeoNavigateSuccess(position: GeolocationPosition) {
+	const { latitude, longitude } = position.coords as GeolocationCoordinates;
+	// const fetchResponseData = await fetchUserCurrentLocation(
+	// 	geoDataURLEndpoint,
+	// 	latitude,
+	// 	longitude
+	// );
+	// setupUsersGeoDataUI(fetchResponseData);
+}
 
 function handleGeoNavigateFailure() {
 	console.log("Error: Unable to access user's current location coordinates");
+}
+
+function setupUsersGeoDataUI(responseData: z.infer<typeof GeoData>) {
+	const { address: userLocationAddress } = responseData;
+	for (const objectKey in userLocationAddress) {
+		if (Object.keys(userLocationAddress).includes(objectKey)) {
+			const HTMLGeoEntityTemplate = `
+			<div class="geolocation__entity">
+				<h4>${objectKey}</h4>
+				<h3>${userLocationAddress[objectKey as keyof typeof userLocationAddress]}</h3>
+			</div>
+		`;
+			const geolocationDivEntity = document
+				.createRange()
+				.createContextualFragment(HTMLGeoEntityTemplate);
+			DOMGeolocationContainer.appendChild(geolocationDivEntity);
+		}
+	}
 }
