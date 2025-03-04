79c7ccbd055587971bae61b541c76e8d32b29b35
sunil <sunil.shastry69@gmail.com>
2025-03-03 20:31:51 -0600

    Import geoOverlayController source file to script.ts source file

--- src/script.ts
+++ src/script.ts
@@ -2,4 +2,5 @@ import './sass/styles.scss';
 
 // import './controllers/quoteController';
 import './controllers/timeController';
+import './controllers/geoOverlayController';
 import './controllers/geoDataController';
