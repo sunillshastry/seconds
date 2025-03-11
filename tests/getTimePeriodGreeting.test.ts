7fd9976f0efcea11feb77c00b03a560f75f32e71
sunil <sunil.shastry69@gmail.com>
2025-03-10 21:25:28 -0600

    Update test title/message for test1 and test2

--- tests/getTimePeriodGreeting.test.ts
+++ tests/getTimePeriodGreeting.test.ts
@@ -2,11 +2,11 @@ import { getTimePeriodGreeting } from './../src/utils/time';
 import { expect, test } from 'vitest';
 
 // Testing for 'morning' times
-test('Testing getTimePeriodGreeting(): morning (1/3)', function () {
+test('src/utils/time.ts: getTimePeriodGreeting(): morning (1/3)', function () {
 	expect(getTimePeriodGreeting(1)).toBe('morning');
 });
 
-test('Testing getTimePeriodGreeting(): morning (2/3)', function () {
+test('src/utils/time.ts: getTimePeriodGreeting(): morning (2/3)', function () {
 	expect(getTimePeriodGreeting(0)).toBe('morning');
 });
 
