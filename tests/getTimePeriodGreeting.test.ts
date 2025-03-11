201cc9e2b35195e6dc91cca325bfee12c53a1fc8
sunil <sunil.shastry69@gmail.com>
2025-03-10 21:20:32 -0600

    Add doc comment before testing

--- tests/getTimePeriodGreeting.test.ts
+++ tests/getTimePeriodGreeting.test.ts
@@ -1,6 +1,7 @@
 import { getTimePeriodGreeting } from './../src/utils/time';
 import { expect, test } from 'vitest';
 
+// Testing for 'morning' times
 test('Testing getTimePeriodGreeting(): morning', function () {
 	expect(getTimePeriodGreeting(1)).toBe('morning');
 });
