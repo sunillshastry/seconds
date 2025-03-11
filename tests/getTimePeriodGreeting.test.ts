5bef4045a74da4582b1c4808d47fb13e81b92a9d
sunil <sunil.shastry69@gmail.com>
2025-03-10 21:22:02 -0600

    Add second test for getTimePeriodGreeting(): morning

--- tests/getTimePeriodGreeting.test.ts
+++ tests/getTimePeriodGreeting.test.ts
@@ -2,6 +2,10 @@ import { getTimePeriodGreeting } from './../src/utils/time';
 import { expect, test } from 'vitest';
 
 // Testing for 'morning' times
-test('Testing getTimePeriodGreeting(): morning', function () {
+test('Testing getTimePeriodGreeting(): morning (1/3)', function () {
 	expect(getTimePeriodGreeting(1)).toBe('morning');
+});
+
+test('Testing getTimePeriodGreeting(): morning (2/3)', function () {
+	expect(getTimePeriodGreeting(0)).toBe('morning');
 });
