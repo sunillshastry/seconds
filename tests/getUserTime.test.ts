c4f2aeaeababe2ac8b576dcb7c47b88007a2d60c
sunil <sunil.shastry69@gmail.com>
2025-03-12 05:06:58 -0600

    Add first test for getUserTime(): Checking 'date' value type and for null/undefined

--- tests/getUserTime.test.ts
+++ tests/getUserTime.test.ts
@@ -1,5 +1,5 @@
 import { getUserTime } from '../src/utils/time';
-import { expect, test } from 'vitest';
+import { assert, expect, expectTypeOf, test } from 'vitest';
 
 // Testing whether getUserTime method has the 'date' property
 test("src/utils/time.ts: getUserTime() [does method have 'date' property]", function () {
@@ -34,4 +34,12 @@ test("src/utils/time.ts: getUserTime() [does method have the 'isPostMeridian' pr
 // Testing whether getUserTime method has the 'greeting' function property
 test("src/utils/time.ts: getUserTime() [does method have the 'greeting()' property", function () {
 	expect(getUserTime()).toHaveProperty('greeting');
+});
+
+// Checking for null or undefined 'date' values & checking for its type
+test("src/utils/time.ts: getUserTime(): Checking the 'date' type and value for null or undefined", function () {
+	const { date } = getUserTime();
+	expectTypeOf(date).toBeNumber();
+	assert.isDefined(date);
+	assert.isNotNull(date);
 });
