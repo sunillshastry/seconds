9380119b9dd5dd2e727d5bd13f49de8afc0bab1b
sunil <sunil.shastry69@gmail.com>
2025-03-15 00:57:46 -0600

    Re-commit previous commit

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,6 @@
 import './sass/styles.scss';
 
-// import './controllers/quoteController';
+import './controllers/quoteController';
 import './controllers/timeController';
 import './controllers/geoOverlayController';
-import './controllers/geoDataController';
+import './controllers/geodataController';
